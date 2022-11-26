adbde3d922c4c4ad87e04154fc50ec67917a0c9c
glbarreto <55640976+glbarreto@users.noreply.github.com>
2022-11-26 02:50:38 -0300

    Add files via upload

---
+++ strings/testCmp/strcmp.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+int main(int argc, char **argv){
+    if(argc < 2){
+        printf("Você esqueceu o parametro\n");
+        return 1;
+    }
+    int n = atoi(argv[1]);
+    char str1[n][15];
+    char str2[n][15];
+    for(int i = 0; i < n; i++){
+        scanf("Insira as strings: %s, %s", str1[i], str2[i]);
+    }
+    time_t t1, t2;
+    t1 = clock();
+    for(int i = 1; i < n; i++){
+        strcmp(str1[i], str2[i]);
+    }
+    t2 = clock();
+    printf("Tempo de execução: %f\n", (double)(t2 - t1)/CLOCKS_PER_SEC);
+    return 0;
+}
